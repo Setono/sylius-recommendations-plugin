3bed7db9a68c03eb0e68328e2c973158ff1b2a82
Joachim Løvgaard <joachim@loevgaard.dk>
2024-02-27 11:45:58 +0100

    Remove unused configuration option

--- tests/DependencyInjection/SetonoSyliusRecommendationsExtensionTest.php
+++ tests/DependencyInjection/SetonoSyliusRecommendationsExtensionTest.php
@@ -19,20 +19,11 @@ final class SetonoSyliusRecommendationsExtensionTest extends AbstractExtensionTe
         ];
     }
 
-    protected function getMinimalConfiguration(): array
-    {
-        return [
-            'option' => 'option_value',
-        ];
-    }
-
     /**
      * @test
      */
     public function after_loading_the_correct_parameter_has_been_set(): void
     {
         $this->load();
-
-        $this->assertContainerBuilderHasParameter('setono_sylius_recommendations.option', 'option_value');
     }
 }
