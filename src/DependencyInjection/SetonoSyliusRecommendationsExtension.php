9da9ca4dadf53b22864f38aaaf1a756c3837daf2
Joachim Løvgaard <joachim@loevgaard.dk>
2024-03-05 15:36:04 +0100

    Remove configuration as it wasn't used

--- src/DependencyInjection/SetonoSyliusRecommendationsExtension.php
+++ src/DependencyInjection/SetonoSyliusRecommendationsExtension.php
@@ -13,14 +13,7 @@ final class SetonoSyliusRecommendationsExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        /**
-         * @psalm-suppress PossiblyNullArgument
-         *
-         * @var array{option: scalar} $config
-         */
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-
         $loader->load('services.xml');
     }
 }
